cce3088d945603f030de14c82a4a4152c3defab6
Truong Luu <dinhluu1259@gmail.com>
2026-03-19 23:06:05 +0700

    feat(auth): add login API

---
+++ toeic-service/src/main/java/com/toeic/be/toeicservice/controller/AuthenticationController.java
@@ -0,0 +1,27 @@
+package com.toeic.be.toeicservice.controller;
+
+import com.toeic.be.toeicservice.dto.request.AuthenticationRequest;
+import com.toeic.be.toeicservice.dto.response.ApiResponse;
+import com.toeic.be.toeicservice.dto.response.AuthenticationResponse;
+import com.toeic.be.toeicservice.service.AuthenticationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthenticationController {
+    @Autowired
+    private AuthenticationService authenticationService;
+
+    @PostMapping("/login")
+    ApiResponse<AuthenticationResponse> authenticate(@RequestBody AuthenticationRequest request) {
+        var results = authenticationService.authenticate(request);
+        return ApiResponse.<AuthenticationResponse>builder()
+                .results(results)
+                .build();
+    }
+
+    }
---
+++ toeic-service/src/main/java/com/toeic/be/toeicservice/dto/request/AuthenticationRequest.java
@@ -0,0 +1,15 @@
+package com.toeic.be.toeicservice.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class AuthenticationRequest {
+    private String username;
+    private String password;
+}
