7b6c7f5397dddf4ff24b07a78112df8d0a2f0ef8
Truong Luu <dinhluu1259@gmail.com>
2026-03-21 23:29:11 +0700

    add get list all users API

--- toeic-service/src/main/java/com/toeic/be/toeicservice/controller/UserController.java
+++ toeic-service/src/main/java/com/toeic/be/toeicservice/controller/UserController.java
@@ -3,18 +3,29 @@ package com.toeic.be.toeicservice.controller;
 import com.toeic.be.toeicservice.dto.request.UserCreationRequest;
 import com.toeic.be.toeicservice.entity.User;
 import com.toeic.be.toeicservice.service.UserService;
+import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("/users")
 public class UserController {
     @Autowired
     private UserService userService;
 
-    @PostMapping("/users")
+    @PostMapping
     User createUser(@RequestBody UserCreationRequest request) {
         return userService.createUser(request);
     }
+
+    @GetMapping
+    List<User> GetUsers(){
+        return userService.getUsers();
+    }
+
+    
+
 }
