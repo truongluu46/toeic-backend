b317bf60d43b74bd75ec0d17a2521b1d52972a0d
Truong Luu <dinhluu1259@gmail.com>
2026-04-14 23:23:45 +0700

    update response for user api

--- toeic-service/src/main/java/com/toeic/be/toeicservice/dto/request/UserUpdateRequest.java
+++ toeic-service/src/main/java/com/toeic/be/toeicservice/dto/request/UserUpdateRequest.java
@@ -9,6 +9,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.Set;
+
 @Getter
 @Setter
 @NoArgsConstructor
@@ -25,5 +27,5 @@ public class UserUpdateRequest {
     private String fullName;
 
     @NotNull
-    private Integer roles;
+    private Set<Integer> roles;
 }
